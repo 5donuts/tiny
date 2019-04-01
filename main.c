f48e225704e2988920dea5f112bb9f6f97cf0b7d
5donuts <5donuts@users.noreply.github.com>
2019-03-31 20:41:26 -0400

    added definitions for put_symbol and get_symbol

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 
 extern FILE *yyin;
 
+// initalize the symbol table
+symbol *symtab = NULL;
+
 int main(int argc, char **argv) {
   ++argv, --argc; // skip over program name
   // either open file specified by command-line argument
@@ -26,4 +29,31 @@ int main(int argc, char **argv) {
   // cleanup & exit
   fclose(out);
   return 0;
+}
+
+// add a symbol to the symbol table
+// NB: the symbol table is LIFO
+symbol *put_symbol(char *name) {
+  // build the new symbol struct
+  symbol *sym = (symbol *) malloc(sizeof(symbol));
+  sym->name = (char *) malloc(strlen(name) + 1);
+  strcpy(sym->name, name);
+
+  // add the new symbol at the beginning of the list
+  sym->next = (struct symbol *) symtab;
+  symtab = sym;
+
+  // return a pointer to the symbol
+  return sym;
+}
+
+// perform a linear search for a symbol with the given name
+// NB: returns either the symbol or NULL
+symbol *get_symbol(char *name) {
+  symbol *sym;
+  for (sym = symtab; sym != NULL; sym = (symbol *) sym->next) {
+    if (strcmp(sym->name, name) == 0)
+      return sym;
+  }
+  return NULL;
 }
