3e7bf1595ce1a20d4e4c09da2ddcd0f5c6800e0c
5donuts <5donuts@users.noreply.github.com>
2019-04-01 21:40:31 -0400

    added typedef for symbol table struct

--- tiny.h
+++ tiny.h
@@ -3,22 +3,29 @@
 #define TINY_H
 
 // imports
+// TODO only have imports that are actually necessary here
+// don't need to bloat tiny.h with #include's
 #include <stdio.h> // for I/O
 #include <stdlib.h> // malloc/free
 #include <string.h> // memcpy/etc.
 
 #include "parser.tab.h" // parser definitions
 
-// new types/structures
-typedef struct symbol { // defines a symbol in the symbol table (linked list)
+// defines valid types for symbols in the symbol table
+typedef enum { VAR, FUNC } symtype;
+
+// defines a symbol in the symbol table
+typedef struct symrec {
   char *name; // name of the symbol
-  struct symbol *next; // next item in linked list
-} symbol;
+  symtype type; // type of the symbol (VAR or FUNC)
+  struct symrec *next; // next item in linked list
+} symrec;
 
 // common function declarations
 int yylex();
 void yyerror(char *);
-symbol *put_symbol(char *); // add a symbol to the symbol table
-symbol *get_symbol(char *); // get a symbol from the symbol table
+void emit(char *); // emit a string of symbols
+symrec *put_symbol(char *); // add a symbol to the symbol table
+symrec *get_symbol(char *); // get a symbol from the symbol table
 
 #endif // TINY_H
