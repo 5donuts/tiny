3651e1c3d1aad15159c5767288705e0e717055f1
5donuts <5donuts@users.noreply.github.com>
2019-04-03 21:44:22 -0400

    contains struct definitions & function declarations for symbol table management

---
+++ symtab.h
@@ -0,0 +1,19 @@
+#ifndef SYMTAB_H
+#define SYMTAB_H
+
+// defines valid types for symbols in the symbol table
+typedef enum { VAR, FUNC } symtype;
+
+// defines a record in the symbol table
+typedef struct symrec {
+  char *name; // name of the symbol
+  symtype type; // type of the symbol (VAR or FUNC)
+  struct symrec *next; // next item in the linked list
+} symrec;
+
+// function declarations
+// NB: functions are defined in main.c
+symrec *putsym(char *); // add a symbol to the symbol table
+symrec *getsym(char *); // retrieve a symbol from the symbol table
+
+#endif // SYMTAB_H
