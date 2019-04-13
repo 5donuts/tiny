b99fa91434831b733dc5af6b16622253800f1e04
5donuts <5donuts@users.noreply.github.com>
2019-04-13 15:52:15 -0400

    messed with sys_exit

--- target_templates/syscalls/sys_exit.s
+++ target_templates/syscalls/sys_exit.s
@@ -1,7 +1,7 @@
-.text
-  .global _start
+ .text
+   .global _start
 
-_start:
-  movl  $1, %eax  # use the _exit syscall
-  movl  $2, %ebx  # exit with status code 2
-  int   $0x80     # make the syscall
+ _start:
+   movl  $1, %eax  # use the _exit syscall
+   movl  $2, %ebx  # exit with status code 2
+   int   $0x80     # make the syscall
