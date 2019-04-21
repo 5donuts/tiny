d282d225df33900d6ae23495a57492cd11c22c27
5donuts <5donuts@users.noreply.github.com>
2019-04-20 20:15:20 -0400

    updated symbol table implementation

--- symtab.c
+++ symtab.c
@@ -6,29 +6,99 @@
 // the symbol table
 symrec *symtab = NULL;
 
-// add a symbol to the symbol table
-// NB: the symbol table is LIFO
-symrec *putsym(char *name) {
-  // build the new symbol struct
-  symrec *sym = (symrec *) malloc(sizeof(symrec));
-  sym->name = (char *) malloc(strlen(name) + 1);
-  strcpy(sym->name, name);
+// prototypes for local functions
+static void put(symrec *); // add a symbol to the table
+static void del(symrec *); // delete a symbol from the table
+static void free_rec(symrec *); // completely free a record in the symbol table
 
-  // add the new symbol at the beginning of the list
-  sym->next = (struct symrec *) symtab;
-  symtab = sym;
-
-  // return a pointer to the symbol
-  return sym;
-}
-
-// perform a linear search for a symbol with the given name
-// NB: returns either the symbol or NULL
-symrec *getsym(char *name) {
+/*
+ * Perform a lookup in the symbol table for a symbol with "name"
+ * If the node exists, return it. Otherwise, create a new node
+ * in the symbol table for the specified name and return it
+ */
+symrec *lookup(char *name) {
   symrec *sym;
-  for (sym = symtab; sym != NULL; sym = (symrec *) sym->next) {
+
+  // get the symbol if it already exists
+  for (sym = symtab; sym != NULL; sym = sym->next) {
     if (strcmp(sym->name, name) == 0)
       return sym;
   }
-  return NULL;
+
+  // make the symbol if it doesn't exist
+  sym = malloc(sizeof(symrec));
+  memset(sym, 0, sizeof(symrec));
+
+  sym->name = (char *) malloc(strlen(name) + 1);
+  strcpy(sym->name, name);
+  put(sym);
+
+  return sym;
+}
+
+// build an empty argument list
+func_arglist *make_empty_arglist() {
+  func_arglist *list = malloc(sizeof(func_arglist));
+  memset(list, 0, sizeof(func_arglist));
+
+  list->name = NULL;
+  list->arg = VOID;
+  list->next = NULL;
+
+  return list;
+}
+
+// add a node to the symbol table in O(1)
+static void put(symrec *sym) {
+  sym->next = symtab;
+  symtab = sym;
+}
+
+// remove a node from the symbol table in O(1)
+static void del(symrec *sym) {
+  symrec *next = sym->next;
+
+  // handle case of NULL pointer
+  if (!next) {
+    free_rec(sym);
+    sym = NULL;
+  }
+
+  // handle case of non-NULL pointer
+  sym->name = next->name;
+  sym->type = next->type;
+  sym->ret_type = next->ret_type;
+  sym->dat_type = next->dat_type;
+  sym->arglist = next->arglist;
+  sym->next = next->next;
+
+  free_rec(next);
+}
+
+// free a record in the symbol table, as well as all of its elements
+static void free_rec(symrec *sym) {
+  free(sym->name); // free symbol name
+
+  // free symbol arglist (if it exists)
+  func_arglist *tmp;
+  for(func_arglist *al = sym->arglist; al != NULL;) {
+    tmp = al;
+    al = al->next;
+    free(tmp);
+  }
+
+  // don't free the pointer to the next struct, that's the responsibility of
+  // the caller to handle
+
+  free(sym);
+}
+
+// free the entire symbol table
+void free_table() {
+  symrec *tmp;
+  for (symrec *sym = symtab; sym != NULL;) {
+    tmp = sym;
+    sym = sym->next;
+    free_rec(tmp);
+  }
 }
--- symtab.h
+++ symtab.h
@@ -4,15 +4,27 @@
 // defines valid types for symbols in the symbol table
 typedef enum { VAR, FUNC } symtype;
 
+// types allowed for variables & function return types
+enum type { INT, VOID };
+typedef struct func_arglist {
+  char *name; // name of the symbol used for this arg
+  enum type arg; // the type of this arg
+  struct func_arglist *next; // next argument (or NULL)
+} func_arglist;
+
 // defines a record in the symbol table
 typedef struct symrec {
   char *name; // name of the symbol
-  symtype type; // type of the symbol (VAR or FUNC)
+  symtype type; // type of the symbol
+  enum type ret_type; // for functions
+  enum type dat_type; // for variables
+  func_arglist *arglist; // argument list for functions
   struct symrec *next; // next item in the linked list
 } symrec;
 
 // function prototypes
-symrec *putsym(char *); // add a symbol to the symbol table
-symrec *getsym(char *); // retrieve a symbol from the symbol table
+symrec *lookup(char *);
+func_arglist *make_empty_arglist();
+void free_table();
 
 #endif // SYMTAB_H
