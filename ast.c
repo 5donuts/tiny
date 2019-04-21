3900eca18af270446e5c5788e809b1983e6ffaa0
5donuts <5donuts@users.noreply.github.com>
2019-04-20 20:13:18 -0400

    added initial implementation of AST

---
+++ ast.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ast.h"
+
+ast_node *ast_root; // root of the AST
+
+// NB: see https://efxa.org/2014/05/25/how-to-create-an-abstract-syntax-tree-while-parsing-an-input-stream/
+
+ast_node *new_ast_node(int node_type, ast_node *left, ast_node *right) {
+  ast_node *node = malloc(sizeof(ast_node));
+  memset(node, 0, sizeof(ast_node));
+
+  node->node_type = node_type;
+  node->left = left;
+  node->right = right;
+
+  return node;
+}
+
+ast_node *new_ast_symbol_reference_node(symrec *symbol) {
+  ast_symbol_reference_node *node = malloc(sizeof(ast_symbol_reference_node));
+  memset(node, 0, sizeof(ast_symbol_reference_node));
+
+  node->node_type = SYMBOL_REFERENCE_NODE;
+  node->symbol = symbol;
+
+  return (ast_node *) node;
+}
+
+ast_node *new_ast_assignment_node(symrec *symbol, ast_node *value) {
+  ast_assignment_node *node = malloc(sizeof(ast_assignment_node));
+  memset(node, 0, sizeof(ast_assignment_node));
+
+  node->node_type = ASSIGNMENT_NODE;
+  node->symbol = symbol;
+  node->value = value;
+
+  return (ast_node *) node;
+}
+
+ast_node *new_ast_number_node(long int value) {
+  ast_number_node *node = malloc(sizeof(ast_number_node));
+  memset(node, 0, sizeof(ast_number_node));
+
+  node->node_type = NUMBER_NODE;
+  node->value = value;
+
+  return (ast_node *) node;
+}
+
+ast_node *new_ast_function_node(symrec *symbol, ast_node *arguments) {
+  ast_function_node *node = malloc(sizeof(ast_function_node));
+  memset(node, 0, sizeof(ast_function_node));
+
+  node->node_type = FUNCTION_NODE;
+  node->symbol = symbol;
+  node->arguments = arguments;
+
+  return (ast_node *) node;
+}
+
+ast_node *new_ast_function_def_node(symrec *symbol, ast_node *arguments,
+                                    ast_node *body) {
+  ast_function_def_node *node = malloc(sizeof(ast_function_def_node));
+  memset(node, 0, sizeof(ast_function_def_node));
+
+  node->node_type = FUNCTION_DEF_NODE;
+  node->symbol = symbol;
+  node->arguments = arguments;
+  node->body = body;
+
+  return (ast_node *) node;
+}
+
+ast_node *new_ast_while_node(ast_node *condition, ast_node *while_branch) {
+  ast_while_node *node = malloc(sizeof(ast_while_node));
+  memset(node, 0, sizeof(ast_while_node));
+
+  node->node_type = WHILE_NODE;
+  node->condition = condition;
+  node->while_branch = while_branch;
+
+  return (ast_node *) node;
+}
+
+// recursively deallocate memory used by the tree
+void free_ast_tree(ast_node *tree) {
+  if (!tree) return; // handle case of NULL pointer
+
+  switch (tree->node_type) {
+    // two sub-trees
+    case WHILE_NODE:
+    case ADD_NODE:
+    case SUB_NODE:
+    case MUL_NODE:
+    case DIV_NODE:
+    case MOD_NODE:
+    case LIST_NODE:
+      free_ast_tree(tree->right);
+      // fall through to one-subtree cases
+
+    // one sub-tree
+    case UMINUS_NODE:
+    case BNEG_NODE:
+    // case RETURN_NODE:
+      free_ast_tree(tree->left);
+      // fall through to no-subtree cases
+
+    // no sub-trees
+    case SYMBOL_REFERENCE_NODE:
+    case NUMBER_NODE:
+      break;
+
+    // special cases
+    case ASSIGNMENT_NODE:
+      {
+        ast_assignment_node *node = (ast_assignment_node *) tree;
+        free_ast_tree(node->value);
+      }
+      break;
+    case FUNCTION_NODE:
+      {
+        ast_function_node *node = (ast_function_node *) tree;
+        free_ast_tree(node->arguments);
+      }
+      break;
+    case FUNCTION_DEF_NODE:
+      {
+        ast_function_def_node *node = (ast_function_def_node *) tree;
+        free_ast_tree(node->arguments);
+        free_ast_tree(node->body);
+      }
+      break;
+
+    // bad node type
+    default:
+      fprintf(stderr, "Error freeing ast_node of type %c\n",
+              tree->node_type);
+      break;
+  }
+
+  free(tree);
+}
--- ast.h
+++ ast.h
@@ -1,6 +1,84 @@
 #ifndef AST_H
 #define AST_H
 
+#include "symtab.h"
 
+// NB: see https://efxa.org/2014/05/25/how-to-create-an-abstract-syntax-tree-while-parsing-an-input-stream/
+
+// node types
+#define SYMBOL_REFERENCE_NODE 'S'
+#define ASSIGNMENT_NODE 'A'
+#define NUMBER_NODE 'N'
+#define FUNCTION_NODE 'F'
+#define FUNCTION_DEF_NODE 'D'
+#define LIST_NODE 'L'
+// #define RETURN_NODE 'R'
+#define WHILE_NODE 'W'
+#define ADD_NODE '+'
+#define SUB_NODE '-'
+#define MUL_NODE '*'
+#define DIV_NODE '/'
+#define MOD_NODE '%'
+#define UMINUS_NODE '_' /* unary negation */
+#define BNEG_NODE '~'   /* bitwise negation */
+// #define LNEG_NODE '!'   /* logical negation */
+
+// node for binary/unary operators & expression lists
+typedef struct ast_node {
+  char node_type;
+  struct ast_node *left;
+  struct ast_node *right;
+} ast_node;
+
+// node for symbol references
+typedef struct ast_symbol_reference_node {
+  char node_type;
+  symrec *symbol;
+} ast_symbol_reference_node;
+
+// node for assignment statements
+typedef struct ast_assignment_node {
+  char node_type;
+  symrec *symbol;
+  ast_node *value;
+} ast_assignment_node;
+
+// node to represent constants
+typedef struct ast_number_node {
+  char node_type;
+  long int value;
+} ast_number_node;
+
+// node to represent a function call
+typedef struct ast_function_node {
+  int node_type;
+  ast_node *arguments;
+  symrec *symbol;
+} ast_function_node;
+
+// node to represent a function definition
+typedef struct ast_function_def_node {
+  int node_type;
+  ast_node *arguments; // TODO capture these in symrec arglist??
+  ast_node *body;
+  symrec *symbol;
+} ast_function_def_node;
+
+// node to represent a while-loop
+typedef struct ast_while_node {
+  int node_type;
+  ast_node *condition;
+  ast_node *while_branch;
+} ast_while_node;
+
+// function prototypes
+ast_node *new_ast_node(int, ast_node *, ast_node *);
+ast_node *new_ast_symbol_reference_node(symrec *);
+ast_node *new_ast_assignment_node(symrec *, ast_node *);
+ast_node *new_ast_number_node(long int);
+ast_node *new_ast_function_node(symrec *, ast_node *);
+ast_node *new_ast_function_def_node(symrec *, ast_node *, ast_node *);
+ast_node *new_ast_while_node(ast_node *, ast_node *);
+void free_ast_tree(ast_node *);
 
 #endif // AST_H
