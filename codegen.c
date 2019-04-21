32e835adad8a07a13f1db88f1e065bc841b9252b
5donuts <5donuts@users.noreply.github.com>
2019-04-20 20:16:40 -0400

    removed code generation routines to rewrite them

--- codegen.c
+++ codegen.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "codegen.h"
 
 extern FILE *out; // the output file (see main.c)
+// TODO write to intermediate file first?
+// yeah, write to an intermediate file for pass 1
+// then the output file for pass 2
+
+// used to keep track of what registers are free
+#define NUM_REGS 4
+static char reg_names[NUM_REGS];
+static bool reg_values[NUM_REGS];
+
+void setup_for_codegen() {
+  // make boilerplate assembly to start
+  fprintf(out, ".text\n");
+  fprintf(out, "\t.global _start\n\n");
+  fprintf(out, "_start:\n");
+
+  // init reg_names & reg_values
+  reg_names = { "%%eax", "%%ebx", "%%ecx", "%%edx" };
+  for (int i = 0; i < NUM_REGS; ++i)
+    reg_values[i] = NULL;
+}
 
 // generate assembly code for system calls
 void make_syscall(syscall *call) {
+  // TODO save register values somewhere?
   // put arguments in appropriate registers depending on the syscall
   switch (call->code) {
     case SYS_EXIT:
@@ -18,4 +40,16 @@ void make_syscall(syscall *call) {
   }
   // make the syscall
   fprintf(out, "\tmovl\t$%x, %%eax\n\tint\t$0x80\n", call->code);
+}
+
+void make_unop(unop op, symrec *symbol) {
+  switch(op) {
+    case MINUS:
+      // arithmetic negation (unary minus)
+    case COMPL:
+    case NOT:
+    default:
+      yyerror("Invalid unary operator");
+      break;
+  }
 }
--- codegen.h
+++ codegen.h
@@ -1,22 +1,31 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
 
-// definitions for linux syscalls
+#include "symtab.h"
+
+// definitions for supported linux syscalls
 #define SYS_EXIT 0x01
 
 // struct to hold a syscall code & its arguments
+// TODO rewrite to not have hardcoded values in the assembly
 typedef struct {
   int code; // in %eax
   void *arg1; // in %ebx
   void *arg2; // in %ecx
   void *arg3; // in %edx
-  void *arg4; // in %esi
+  void *arg4; // in %esi // TODO remove these?
   void *arg5; // in %edi
 } syscall;
 
-// function declarations
-// NB: functions are defined in main.c
+// represents valid unary operators
+typedef enum {
+  MINUS, // unary negation
+  COMPL, // one's complement
+  NOT // logical NOT
+} unop;
+
+// function prototypes
 void make_syscall(syscall *); // make a syscall with some arguments
-// TODO write more declarations & implement these
+void make_unop(unop, symrec *); // generate assembly to apply a unary operator
 
 #endif // CODEGEN_H
